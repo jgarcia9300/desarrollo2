9425a370cb716f0ced174eb92c8bfc0ee82f62a6
jgarcia9300 <juan.m.garcia@correounivalle.edu.co>
2024-12-02 01:32:23 -0500

    configurando sonarcloud

--- jest.config.cjs
+++ jest.config.cjs
@@ -11,4 +11,9 @@ module.exports = {
   transformIgnorePatterns: [
     'node_modules/(?!(puppeteer|bootstrap)/)',
   ],
+  collectCoverage: true,  // Asegúrate de que la recopilación de cobertura esté habilitada
+  coverageDirectory: 'coverage',  // El directorio donde se almacenarán los informes de cobertura
+  coverageReporters: ['lcov', 'text'],  // Asegúrate de que `lcov` esté en los reporteros
+  coveragePathIgnorePatterns: ['/node_modules/'],  // Ignora los directorios que no te interesen
+  
 };
